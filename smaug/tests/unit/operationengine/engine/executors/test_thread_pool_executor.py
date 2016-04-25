f98c9d6e57916814e97965bebba8db739a38aaf2
zengchen <chenzeng2@huawei.com>
2016-04-25 17:13:19 +0800

    Implement protect operation
    Change-Id: I2e471d01ce2e5147cf44525d52b52d3ff2147167
    Closes-Bug: #1574527

--- smaug/tests/unit/operationengine/engine/executors/test_thread_pool_executor.py
+++ smaug/tests/unit/operationengine/engine/executors/test_thread_pool_executor.py
@@ -12,7 +12,6 @@
 
 from datetime import datetime
 from datetime import timedelta
-import mock
 import time
 
 from smaug.common import constants
@@ -22,37 +21,10 @@ from smaug.services.operationengine.engine.executors import\
     thread_pool_executor
 from smaug.tests import base
 
-NOW = datetime.utcnow()
-Log_ID = 0
-Operation_ID = "123"
 
-Fake_Log = {
-    'created_at': NOW,
-    'deleted_at': None,
-    'updated_at': NOW,
-    'deleted': False,
-    'id': Log_ID,
-    'operation_id': Operation_ID,
-    'expect_start_time': NOW,
-    'triggered_time': NOW,
-    'actual_start_time': NOW,
-    'end_time': NOW,
-    'state': 'in_progress',
-    'extend_info': '',
-}
-
-Fake_Operation = {
-    'created_at': NOW,
-    'deleted_at': None,
-    'updated_at': NOW,
-    'deleted': False,
-    'id': Operation_ID,
-    'name': 'protect vm',
-    'operation_type': 'protect',
-    'project_id': '123',
-    'trigger_id': '0354ca9ddcd046b693340d78759fd275',
-    'operation_definition': '{}'
-}
+class FakeOperationManager(object):
+    def run_operation(self, operation_type, operation_definition, **kwargs):
+        return
 
 
 class ThreadPoolExecutorTestCase(base.TestCase):
@@ -61,15 +33,14 @@ class ThreadPoolExecutorTestCase(base.TestCase):
         super(ThreadPoolExecutorTestCase, self).setUp()
 
         self._executor = thread_pool_executor.ThreadPoolExecutor()
+        self._executor._operation_manager = FakeOperationManager()
         self.context = context.get_admin_context()
 
     def tearDown(self):
         super(ThreadPoolExecutorTestCase, self).tearDown()
         self._executor.shutdown()
 
-    @mock.patch('smaug.services.operationengine.operations.'
-                'protect_operation.ProtectOperation._execute')
-    def test_execute_operation(self, execute):
+    def test_execute_operation(self):
         operation = self._create_operation()
         self._create_operation_state(operation.id, 0)
 
@@ -82,24 +53,12 @@ class ThreadPoolExecutorTestCase(base.TestCase):
 
         self.assertEqual(0, len(self._executor._operation_to_run))
 
-        param = {
-            'operation_id': operation.id,
-            'triggered_time': now,
-            'expect_start_time': now,
-            'window_time': window_time,
-            'run_type': constants.OPERATION_RUN_TYPE_EXECUTE
-        }
-        execute.assert_called_once_with(
-            operation.project_id, operation.operation_definition, param)
-
         state = objects.ScheduledOperationState.get_by_operation_id(
             self.context, operation.id)
         self.assertTrue(state.end_time_for_run is not None)
         self.assertTrue(constants.OPERATION_STATE_REGISTERED == state.state)
 
-    @mock.patch('smaug.services.operationengine.operations.'
-                'protect_operation.ProtectOperation._resume')
-    def test_resume_operation(self, resume):
+    def test_resume_operation(self):
         operation = self._create_operation()
         self._create_operation_state(operation.id, 0)
 
@@ -113,8 +72,6 @@ class ThreadPoolExecutorTestCase(base.TestCase):
 
         self.assertEqual(0, len(self._executor._operation_to_run))
 
-        self.assertTrue(resume.called)
-
         state = objects.ScheduledOperationState.get_by_operation_id(
             self.context, operation.id)
         self.assertTrue(constants.OPERATION_STATE_REGISTERED == state.state)
@@ -126,7 +83,7 @@ class ThreadPoolExecutorTestCase(base.TestCase):
         self.assertEqual(0, len(self._executor._operation_to_cancel))
 
         self._executor._operation_to_run[operation_id] = 0
-        self._executor.cancel_operation(Operation_ID)
+        self._executor.cancel_operation(operation_id)
         self.assertEqual(1, len(self._executor._operation_to_cancel))
 
     def _create_operation(self, trigger_id='123'):
