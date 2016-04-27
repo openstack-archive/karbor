51ab22f2cded77edd6d532e70aa39d4ed8de062e
zengchen <chenzeng2@huawei.com>
2016-04-27 17:52:23 +0800

    Encapsulate smaug client in Operation Engine
    According to the design, all the operations which will access
    'protection service' module should invoke the API of Smaug instead.
    Take the protect operation for example, it will POST the requst
    of '/v1/providers/{provider_id}/checkpoints' to execute the
    protect operation. Therefore, I encapsulate the client of Smaug
    to send the all kinds of requsts conveniently.
    
    Change-Id: Ifc06e49df764d6b17b08f2e9d3f2cb9c64e205c0
    Closes-Bug: #1575569

--- karbor/services/operationengine/karbor_client.py
+++ karbor/services/operationengine/karbor_client.py
@@ -10,10 +10,46 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from oslo_config import cfg
+from oslo_log import log as logging
+
+from karbor.common import config
+from karbor.common import karbor_keystone_plugin
+from karborclient import client as karbor_client
+
+LOG = logging.getLogger(__name__)
+
+CONFIG_GROUP = 'karbor_client'
+CONF = cfg.CONF
+CONF.register_opts(config.service_client_opts,
+                   group=CONFIG_GROUP)
+
 
 def get_karbor_endpoint():
-    pass
+    try:
+        sc_cfg = CONF[CONFIG_GROUP]
+        kc_plugin = karbor_keystone_plugin.KarborKeystonePlugin()
+        return kc_plugin.get_service_endpoint(
+            sc_cfg.service_name, sc_cfg.service_type, sc_cfg.region_id)
+    except Exception:
+        raise
 
 
 def create(context, **kwargs):
-    pass
+    endpoint = kwargs.get('endpoint')
+    if not endpoint:
+        endpoint = get_karbor_endpoint()
+        endpoint = endpoint.replace("$(tenant_id)s", context.project_id)
+
+    LOG.debug("Creating karbor client with url %s.", endpoint)
+
+    sc_cfg = CONF[CONFIG_GROUP]
+    args = {
+        'version': sc_cfg.version,
+        'endpoint': endpoint,
+        'token': context.auth_token,
+        'cacert': sc_cfg.ca_cert_file,
+        'insecure': sc_cfg.auth_insecure,
+    }
+
+    return karbor_client.Client(**args)
