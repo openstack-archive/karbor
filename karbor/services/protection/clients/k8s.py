4b531702358643f1b9418c5aa53c05c47c9615ab
chenying <ying.chen@huawei.com>
2017-08-29 20:02:55 +0800

    Add kubernetes client to Karbor
    Change-Id: I7543d29f575ecca9e3aac28c2c7157037731f928
    Implements: blueprint kubernetes-pods-protection-plugin

---
+++ karbor/services/protection/clients/k8s.py
@@ -0,0 +1,54 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from kubernetes import client
+from kubernetes.client import api_client
+from kubernetes.client import ConfigurationObject
+
+from oslo_config import cfg
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+SERVICE = 'k8s'
+kubernetes_client_opts = [
+    cfg.StrOpt(SERVICE + '_host',
+               help='The IP address of the kubernetes api server.'),
+    cfg.StrOpt(SERVICE + '_ssl_ca_cert',
+               help='The certificate authority will be used for secure '
+                    'access from Admission Controllers.'),
+    cfg.StrOpt(SERVICE + '_cert_file',
+               help='The client certificate file for the kubernetes '
+                    'cluster.'),
+    cfg.StrOpt(SERVICE + '_key_file',
+               help='The client key file for the kubernetes cluster.')
+]
+
+
+def register_opts(conf):
+    conf.register_opts(kubernetes_client_opts, group=SERVICE + '_client')
+
+
+def create(context, conf, **kwargs):
+    register_opts(conf)
+
+    client_config = conf.k8s_client
+    LOG.info('Creating the kubernetes client with url %s.',
+             client_config.k8s_host)
+
+    config = ConfigurationObject()
+    config.host = client_config.k8s_host
+    config.ssl_ca_cert = client_config.k8s_ssl_ca_cert
+    config.cert_file = client_config.k8s_cert_file
+    config.key_file = client_config.k8s_key_file
+    k8s_api_client = api_client.ApiClient(config=config)
+    k8s_core_v1_api = client.CoreV1Api(k8s_api_client)
+    return k8s_core_v1_api
---
+++ karbor/tests/unit/clients/test_k8s_client.py
@@ -0,0 +1,47 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_config import cfg
+
+from karbor.context import RequestContext
+from karbor.services.protection.clients import k8s
+from karbor.tests import base
+
+
+class KubernetesClientTest(base.TestCase):
+    def setUp(self):
+        super(KubernetesClientTest, self).setUp()
+        self._context = RequestContext(user_id='demo',
+                                       project_id='abcd',
+                                       auth_token='efgh',
+                                       service_catalog=None)
+
+        self.conf = cfg.ConfigOpts()
+        k8s.register_opts(self.conf)
+        self.host_url = 'https://192.168.98.35:6443'
+        self.conf.set_default('k8s_host',
+                              self.host_url,
+                              'k8s_client')
+        self.conf.set_override('k8s_ssl_ca_cert',
+                               '/etc/provider.d/server-ca.crt',
+                               'k8s_client')
+        self.conf.set_override('k8s_cert_file',
+                               '/etc/provider.d/client-admin.crt',
+                               'k8s_client')
+        self.conf.set_override('k8s_key_file',
+                               '/etc/provider.d/client-admin.key',
+                               'k8s_client')
+
+    def test_create_client(self):
+
+        client = k8s.create(self._context, self.conf)
+        self.assertEqual(client.api_client.host, self.host_url)
