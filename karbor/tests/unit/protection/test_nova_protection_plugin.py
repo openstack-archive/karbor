a78cfa0270e160f785a592403d23d775bf93d8bb
chenying <ying.chen@huawei.com>
2017-10-25 11:04:01 +0800

    Add verify action for the server backup protection plugin
    Change-Id: I0c45753edd6c0a9ba7bf65d1a58c5c5765cd9bb0
    Implements: blueprint support-verify-the-checkpoint-api

--- karbor/tests/unit/protection/test_nova_protection_plugin.py
+++ karbor/tests/unit/protection/test_nova_protection_plugin.py
@@ -454,6 +454,23 @@ class NovaProtectionPluginTest(base.TestCase):
         call_hooks(delete_operation, self.checkpoint, resource, self.cntxt,
                    {})
 
+    @mock.patch('karbor.services.protection.protection_plugins.utils.'
+                'update_resource_verify_result')
+    def test_verify_backup(self,  mock_update_verify):
+        resource = Resource(id="123",
+                            type=constants.SERVER_RESOURCE_TYPE,
+                            name='fake')
+
+        fake_bank._plugin._objects[
+            "/resource_data/checkpoint_id/123/status"
+        ] = "available"
+
+        verify_operation = self.plugin.get_verify_operation(resource)
+        call_hooks(verify_operation, self.checkpoint, resource, self.cntxt,
+                   {})
+        mock_update_verify.assert_called_with(
+            None, resource.type, resource.id, 'available')
+
     def test_get_supported_resources_types(self):
         types = self.plugin.get_supported_resources_types()
         self.assertEqual([constants.SERVER_RESOURCE_TYPE], types)
